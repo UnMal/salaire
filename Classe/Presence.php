74941f059475b1649686b686608c8b30d3201c01
UnMal <lordvonhymannovitch@gmail.com>
2019-12-25 23:53:33 +0100

    base de données et constructeur présence

--- Classe/Presence.php
+++ Classe/Presence.php
@@ -1,28 +1,30 @@
 <?php
-	class Presence{
-		private $Assiduite, $Debut, $Participation, $Discipline, $Fin, $Date;
+	
+	include_once('bd.php');//connexion a la base de données
 
-		public function __construct($Assiduite, $Debut, $Participation, $Discipline, $Fin, $Total/*nombre total de points*/){
+	class Presence{
+		//déclaration de variables
+		private $Assiduite, $Debut, $Participation, $Discipline, $Fin, $Date;
+		//constructeur avec insertion en base de données
+		public function __construct($Assiduite, $Debut, $Participation, $Discipline, $Fin){
+			/*$timestamp = date('Y-m-d');
+			$Date = $timestamp;*/
 			$this->Assiduite=$Assiduite;
 			$this->Debut=$Debut;
 			$this->Participation=$Participation;
 			$this->Discipline=$Discipline;
 			$this->Fin=$Fin;
-			$this->Total=0;
-			$this->Date=date("d-m-Y");
-		}
+			$this->Date=date('Y-m-d');
 
-		include_once('bd.php');
-
-		$query=$pdo->prepare('INSERT INTO presence VALUES (UserId:=id');
-		$query=$pdo->exec(array(['id'=> $id]));
-		
-		$donne = $query -> fetchAll();
-
-		foreach ($donne as $key => $donnes) {
-			
-		echo " ".$donnes['nom']." ".$donnes['prenom']."";
-
+			$req=$pdo->prepare('INSERT INTO presence (Assiduite, Debut, Participation, Discipline, fin, Date1) VALUES (:Assiduite, :Debut, :Participation, :Discipline, :fin, :Date1)');
+			$req->execute(array(
+				'Assiduite'=>$Assiduite,
+				'Debut'=>$Debut,
+				'Participation'=>$Participation,
+				'Discipline'=>$Discipline,
+				'fin'=>$Fin,
+				'Date1'=>$Date
+			));
 		}
 	}
 ?>
--- Classe/bd.php
+++ Classe/bd.php
@@ -1,18 +1,9 @@
 <?php
-	if (extension_loaded ('PDO')) {
-	echo 'PDO OK'; 
-	} else {
-	echo 'PDO KO'; 
-	}
-
-	$dsn = 'mysql:host=localhost;dbname=Salaire;port=80;';
-	$pdo = new PDO($dsn, 'root', '');
-
 	try {
-			$pdo = new PDO($dsn, 'root' , '');
-		}
-		catch (PDOException $exception) {
-			 mail('', 'PDOException', $exception->getMessage());
-			 exit('Erreur de connexion à la base de données');
-		}
+		$pdo = new PDO('mysql:host=localhost;dbname=salaire;charset=utf8', 'root', '');
+	}
+	catch (Exception $e)
+	{
+	    die('Erreur : ' . $e->getMessage());
+	}
 ?>
