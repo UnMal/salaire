75e03bdc8853c456e043ee6b866a3d6336a98d5c
UnMal <lordvonhymannovitch@gmail.com>
2019-12-25 21:48:13 +0100

    constructeur presence

--- Classe/Presence.php
+++ Classe/Presence.php
@@ -2,12 +2,27 @@
 	class Presence{
 		private $Assiduite, $Debut, $Participation, $Discipline, $Fin, $Date;
 
-		public function __construct($Assiduite, $Debut, $Participation, $Discipline, $Fin){
+		public function __construct($Assiduite, $Debut, $Participation, $Discipline, $Fin, $Total/*nombre total de points*/){
 			$this->Assiduite=$Assiduite;
 			$this->Debut=$Debut;
 			$this->Participation=$Participation;
 			$this->Discipline=$Discipline;
 			$this->Fin=$Fin;
+			$this->Total=0;
+			$this->Date=date("d-m-Y");
+		}
+
+		include_once('bd.php');
+
+		$query=$pdo->prepare('INSERT INTO presence VALUES (UserId:=id');
+		$query=$pdo->exec(array(['id'=> $id]));
+		
+		$donne = $query -> fetchAll();
+
+		foreach ($donne as $key => $donnes) {
+			
+		echo " ".$donnes['nom']." ".$donnes['prenom']."";
+
 		}
 	}
 ?>
